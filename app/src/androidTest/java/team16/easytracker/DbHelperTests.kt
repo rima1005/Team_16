2f9e5b12ccdb3b4f650a0affcf31b5302fad4ef2
Markus Ritzer <markus.ritzer@student.tugraz.at>
2021-04-14 12:22:04 +0200

    EasyTracker-123 Finished SQLite Table Setup + worker tests [PO, RM]

--- app/src/androidTest/java/team16/easytracker/DbHelperTests.kt
+++ app/src/androidTest/java/team16/easytracker/DbHelperTests.kt
@@ -8,7 +8,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import team16.easytracker.database.Contracts.Company
+import team16.easytracker.database.Contracts.Worker
 import team16.easytracker.database.DbHelper
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 @RunWith(AndroidJUnit4::class)
 class DbHelperTests {
@@ -21,6 +24,8 @@ class DbHelperTests {
     val readableDb = dbHelper.readableDatabase
 
     val COMPANY_DUMMY_NAME = "company 1"
+    val WORKER_DUMMY_FIRST_NAME = "Max"
+    val WORKER_DUMMY_LAST_NAME = "Mustermann"
 
     @Before
     fun init() {
@@ -41,11 +46,35 @@ class DbHelperTests {
         return writableDb.insert(Company.TABLE_NAME, null, values)
     }
 
+    private fun insertDummyWorker() : Long {
+
+        val values = ContentValues().apply {
+            put(Worker.COL_FIRST_NAME, WORKER_DUMMY_FIRST_NAME)
+            put(Worker.COL_LAST_NAME, WORKER_DUMMY_LAST_NAME)
+            //put(Worker.COL_DATE_OF_BIRTH, "1998-05-10")
+            put(Worker.COL_TITLE, "")
+            put(Worker.COL_EMAIL, "max.mustermann@gmail.com")
+            put(Worker.COL_PASSWORD, "1235165465")
+            put(Worker.COL_PHONE_NUMBER, "+43 9509579554")
+            //put(Worker.COL_CREATED_AT, DateTimeFormatter.ISO_INSTANT.format(Instant.now()))
+            put(Worker.COL_ADDRESS_ID, 1)
+        }
+
+        return writableDb.insert(Worker.TABLE_NAME, null, values)
+    }
+
     @Test
     fun testInsertCompany() {
         val newRowId = insertDummyCompany()
         assert(newRowId > 0)
     }
+
+    @Test
+    fun testInsertWorker() {
+        val newRowId = insertDummyWorker()
+        assert(newRowId > 0)
+    }
+
 
     @Test
     fun testReadCompany() {
@@ -55,5 +84,17 @@ class DbHelperTests {
         result.moveToFirst()
         val companyName = result.getString(result.getColumnIndexOrThrow(Company.COL_NAME))
         assert(companyName == COMPANY_DUMMY_NAME)
+    }
+
+    @Test
+    fun testReadWorker() {
+        val newRowId = insertDummyWorker()
+        val query = "SELECT * FROM ${Worker.TABLE_NAME} WHERE ${Worker.COL_ID} = ?"
+        val result = readableDb.rawQuery(query, arrayOf(newRowId.toString()))
+        result.moveToFirst()
+        val workerFirstName = result.getString(result.getColumnIndexOrThrow(Worker.COL_FIRST_NAME))
+        assert(workerFirstName == WORKER_DUMMY_FIRST_NAME)
+        val workerLastName = result.getString(result.getColumnIndexOrThrow(Worker.COL_LAST_NAME))
+        assert(workerLastName == WORKER_DUMMY_LAST_NAME)
     }
 }
--- app/src/main/java/team16/easytracker/database/DbHelper.kt
+++ app/src/main/java/team16/easytracker/database/DbHelper.kt
@@ -4,31 +4,81 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import team16.easytracker.database.Contracts.Company
+import team16.easytracker.database.Contracts.Address
+import team16.easytracker.database.Contracts.Worker
+import team16.easytracker.database.Contracts.CompanyWorker
+import team16.easytracker.database.Contracts.Tracking
 
 
 private const val SQL_CREATE_ENTRIES =
-    "CREATE TABLE ${Company.TABLE_NAME} (" +
+    "CREATE TABLE IF NOT EXISTS ${Company.TABLE_NAME} (" +
             "${Company.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
             "${Company.COL_NAME} VARCHAR(256)," +
-            "${Company.COL_ADDRESS_ID} INTEGER)"
+            "${Company.COL_ADDRESS_ID} INTEGER);" +
+
+    "CREATE TABLE IF NOT EXISTS ${Address.TABLE_NAME} (" +
+            "${Address.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "${Address.COL_STREET} VARCHAR(256)," +
+            "${Address.COL_ZIP_CODE} VARCHAR(16)," +
+            "${Address.COL_CITY} VARCHAR(64));" +
+
+    "CREATE TABLE IF NOT EXISTS ${Worker.TABLE_NAME} (" +
+            "${Worker.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "${Worker.COL_FIRST_NAME} VARCHAR(128)," +
+            "${Worker.COL_LAST_NAME} VARCHAR(128)," +
+            "${Worker.COL_DATE_OF_BIRTH} DATE," +
+            "${Worker.COL_TITLE} VARCHAR(64)," +
+            "${Worker.COL_EMAIL} VARCHAR(128)," +
+            "${Worker.COL_PASSWORD} VARCHAR(128)," +
+            "${Worker.COL_PHONE_NUMBER} VARCHAR(32)," +
+            "${Worker.COL_CREATED_AT} TIMESTAMP," +
+            "${Worker.COL_ADDRESS_ID} INTEGER);" +
+
+    "CREATE TABLE IF NOT EXISTS ${CompanyWorker.TABLE_NAME} (" +
+            "${CompanyWorker.COL_COMPANY_ID} INTEGER," +
+            "${CompanyWorker.COL_WORKER_ID} INTEGER," +
+            "${CompanyWorker.COL_POSITION} VARCHAR(128));" +
+
+    "CREATE TABLE IF NOT EXISTS ${Tracking.TABLE_NAME} (" +
+            "${Tracking.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "${Tracking.COL_NAME} VARCHAR(128)," +
+            "${Tracking.COL_WORKER_ID} INTEGER," +
+            "${Tracking.COL_START_TIME} DATETIME," +
+            "${Tracking.COL_END_TIME} DATETIME," +
+            "${Tracking.COL_DESCRIPTION} TEXT," +
+            "${Tracking.COL_BLUETOOTH_DEVICE} VARCHAR(128));"
+
+private const val SQL_CREATE_WORKERS =
+        "CREATE TABLE IF NOT EXISTS ${Worker.TABLE_NAME} (" +
+        "${Worker.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "${Worker.COL_FIRST_NAME} VARCHAR(128)," +
+        "${Worker.COL_LAST_NAME} VARCHAR(128)," +
+        "${Worker.COL_DATE_OF_BIRTH} DATE," +
+        "${Worker.COL_TITLE} VARCHAR(64)," +
+        "${Worker.COL_EMAIL} VARCHAR(128)," +
+        "${Worker.COL_PASSWORD} VARCHAR(128)," +
+        "${Worker.COL_PHONE_NUMBER} VARCHAR(32)," +
+        "${Worker.COL_CREATED_AT} TIMESTAMP," +
+        "${Worker.COL_ADDRESS_ID} INTEGER)"
 
 class DbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(SQL_CREATE_ENTRIES)
+        //db.execSQL(SQL_CREATE_WORKERS)
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // This database is only a cache for online data, so its upgr   ade policy is
         // to simply to discard the data and start over
-        // db.execSQL(SQL_DELETE_ENTRIES)
-        // onCreate(db)
+        //db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
     }
     override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        // onUpgrade(db, oldVersion, newVersion)
+        onUpgrade(db, oldVersion, newVersion)
     }
     companion object {
         // If you change the database schema, you must increment the database version.
-        const val DATABASE_VERSION = 1
+        const val DATABASE_VERSION = 4
         const val DATABASE_NAME = "EasyTracker.db"
     }
 }
