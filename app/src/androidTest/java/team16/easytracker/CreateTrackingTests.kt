1906a19ac9d5018d75f403165565b293db277ba4
Sebastian Guertl <sebastian.guertl@gmail.com>
2021-04-30 15:40:56 +0200

    ET-127 Add tests for empty input, valid input [GS,WF]

--- app/src/androidTest/java/team16/easytracker/CreateTrackingTests.kt
+++ app/src/androidTest/java/team16/easytracker/CreateTrackingTests.kt
@@ -402,4 +402,104 @@ class CreateTrackingTests {
                 .check(matches(withText("The tracking name is required")))
     }
 
+    @Test
+    fun emptyInputCreateTracking() {
+        val currentActivity : HomeActivity = getCurrentActivity() as HomeActivity
+
+        val createTrackingFragment = Trackings()
+        currentActivity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.flFragment, createTrackingFragment, "CreateTrackingFragment")
+                .addToBackStack(null)
+                .commit()
+
+        onView(withId(R.id.btnCreateTracking))
+                .perform(click())
+
+        onView(withId(R.id.tvErrorTrackingStartDate))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.tvErrorTrackingStartTime))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.tvErrorTrackingEndDate))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.tvErrorTrackingEndTime))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.tvErrorTrackingName))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.btnCreateTrackingBack))
+                .perform(scrollTo())
+
+        onView(withId(R.id.btnCreateTrackingSave))
+                .perform(click())
+
+        onView(withId(R.id.tvTrackingStartDate))
+                .perform(scrollTo())
+        onView(withId(R.id.tvErrorTrackingStartDate))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("The start date is required")))
+
+        onView(withId(R.id.tvTrackingStartTime))
+                .perform(scrollTo())
+        onView(withId(R.id.tvErrorTrackingStartTime))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("The start time is required")))
+
+        onView(withId(R.id.tvTrackingEndDate))
+                .perform(scrollTo())
+        onView(withId(R.id.tvErrorTrackingEndDate))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("The end date is required")))
+
+        onView(withId(R.id.tvTrackingEndTime))
+                .perform(scrollTo())
+        onView(withId(R.id.tvErrorTrackingEndTime))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("The end time is required")))
+
+        onView(withId(R.id.tvLabelTrackingName))
+                .perform(scrollTo())
+        onView(withId(R.id.tvErrorTrackingName))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("The tracking name is required")))
+    }
+
+    @Test
+    fun validCreateTracking() {
+        val currentActivity : HomeActivity = getCurrentActivity() as HomeActivity
+
+        val createTrackingFragment = Trackings()
+        currentActivity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.flFragment, createTrackingFragment, "CreateTrackingFragment")
+                .addToBackStack(null)
+                .commit()
+
+        onView(withId(R.id.btnCreateTracking))
+                .perform(click())
+
+        onView(withId(R.id.etTrackingStartDate))
+                .perform(typeText("28.04.2021"), closeSoftKeyboard())
+
+        onView(withId(R.id.etTrackingStartTime))
+                .perform(typeText("10:00"), closeSoftKeyboard())
+
+        onView(withId(R.id.etTrackingEndDate))
+                .perform(typeText("28.04.2021"), closeSoftKeyboard())
+
+        onView(withId(R.id.etTrackingEndTime))
+                .perform(typeText("14:00"), closeSoftKeyboard())
+
+        onView(withId(R.id.etTrackingName))
+                .perform(typeText("Some example tracking"), closeSoftKeyboard())
+
+        onView(withId(R.id.btnCreateTrackingBack))
+                .perform(scrollTo())
+
+        onView(withId(R.id.btnCreateTrackingSave))
+                .perform(click())
+    }
+
 }
