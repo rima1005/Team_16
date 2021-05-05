103c9f1315287b50108f675e0ba460e3d53f691f
redhawk0799 <selimi.flakon@gmail.com>
2021-05-05 09:11:41 +0200

    ET-131 added file for "start tracking time" [GS, SF]

---
+++ app/src/androidTest/java/team16/easytracker/TrackTimeTests.kt
@@ -0,0 +1,43 @@
+package team16.easytracker
+
+import android.app.Activity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TrackTimeTests {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    fun getCurrentActivity(): Activity? {
+        var currentActivity: Activity? = null
+        InstrumentationRegistry.getInstrumentation()
+            .runOnMainSync { run { currentActivity = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
+                Stage.RESUMED).elementAtOrNull(0) } }
+        return currentActivity
+    }
+
+    @Ignore
+    @Test
+    fun startTracking(){
+        onView(withId(R.id.btnStartTracking))
+            .perform(click())
+
+    }
+
+    @Test
+    fun closeTracking() {
+
+    }
+}
